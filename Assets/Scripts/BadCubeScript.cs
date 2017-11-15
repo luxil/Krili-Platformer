39c4cc42ae2e94f26d3f315f70566dfd55b91d48
INTERCEPT\linh.do <linh.do@intercept.de>
2017-11-15 21:41:59 +0100

    Healthbar V0.1

---
+++ Assets/Scripts/BadCubeScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadCubeScript : MonoBehaviour {
+
+    //https://www.mpgh.net/forum/showthread.php?t=427244
+
+    public GameObject gameControl;
+    GameControlScript gameControlScript;
+    public float collisionTime;
+
+    // Use this for initialization
+    void Start () {
+        gameControlScript = gameControl.GetComponent<GameControlScript>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+            gameControlScript.reduceHearts(1);
+
+        //foreach (ContactPoint contact in collision.contacts)
+        //{
+        //    Debug.DrawRay(contact.point, contact.normal, Color.white);
+        //    gameControlScript.reduceHearts(1);
+        //}
+    }
+
+}
---
+++ Assets/Scripts/GameControlScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControlScript : MonoBehaviour {
+
+    //tutorial from: https://www.youtube.com/watch?v=LsUiJItfzxU
+
+    public GameObject[] hearts;
+    public int health;
+
+    // Use this for initialization
+    void Start()
+    {
+        health = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        setActiveHearts(health);
+    }
+
+    public void addHearts(int numberOfHearts)
+    {
+        health += numberOfHearts;
+    }
+
+    public void reduceHearts(int numberOfHearts)
+    {
+        health -= numberOfHearts;
+    }
+
+    void setActiveHearts(int numberActiveHearts)
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if(i < numberActiveHearts)
+                hearts[i].gameObject.SetActive(true);
+            else
+                hearts[i].gameObject.SetActive(false);
+        }
+    }
+}
