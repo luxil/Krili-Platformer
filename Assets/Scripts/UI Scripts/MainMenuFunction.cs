6b24ebb1691d4fb6b90517fec5aeca3b26263ff3
kr-istin <kr.reich@web.de>
2017-11-29 15:35:29 +0100

    added main menu

---
+++ Assets/Scripts/UI Scripts/MainMenuFunction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuFunction : MonoBehaviour {
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void EndGame()
+    {
+        
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                Application.Quit ();
+        #endif
+    }
+
+}
