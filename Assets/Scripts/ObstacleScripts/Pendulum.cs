589d2fd42ab78edfdd0c1b6f88888ce8a06f280d
kr-istin <kr.reich@web.de>
2017-12-05 22:49:49 +0100

    added basic pendulum

---
+++ Assets/Scripts/ObstacleScripts/Pendulum.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pendulum : MonoBehaviour {
+
+    public float fSpeed = 0.4f;     // speed at which the pendulum swings
+    public float fAmplitude = 50;   // amplitude
+
+    void Update () {
+
+        ///  <summary>
+        ///     Quaternion Euler(float x, float y, float z);
+        ///     Returns a rotation that rotates z degrees around the z axis, x degrees around the x axis, and y degrees around the y axis (in that order).
+        ///  </summary>
+
+        //transform.rotation = Quaternion.Euler(Mathf.Sin(Time.time * fSpeed) * fAmplitude, 0, 0);
+        transform.rotation = Quaternion.Euler(Mathf.Sin(Time.time * Mathf.PI * fSpeed) * fAmplitude, 0, 0);
+
+    }
+}
