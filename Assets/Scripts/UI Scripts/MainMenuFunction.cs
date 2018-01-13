e347a11f21257c46d1eac3d79c6ce429c6e97105
kr-istin <kr.reich@web.de>
2018-01-14 00:37:45 +0100

    changed main menu, added level selection, added more UI assets

--- Assets/Scripts/UI Scripts/MainMenuFunction.cs
+++ Assets/Scripts/UI Scripts/MainMenuFunction.cs
@@ -5,9 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuFunction : MonoBehaviour {
 
+    public GameObject goMainMenuContainer;
+    public GameObject goLevelPanel;
+
+    void Start()
+    {
+        // disable Level selection at the start of the scene
+        goLevelPanel.SetActive(false);
+    }
+
     public void PlayGame()
     {
-        SceneManager.LoadScene("Level01");
+        goMainMenuContainer.SetActive(false);
+        goLevelPanel.SetActive(true);
+    }
+
+    public void LoadLevel(string selectedLevel)
+    {
+        // String has to be the Name of the scene!
+        SceneManager.LoadScene(selectedLevel);
+    }
+
+    public void CloseLevelSelection()
+    {
+        goMainMenuContainer.SetActive(true);
+        goLevelPanel.SetActive(false);
     }
 
     public void EndGame()
