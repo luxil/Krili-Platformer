03c6676fe692d669eab42c7aa9ea7928b1ab2d75
Linh Do <luxiliph@gmail.com>
2018-02-17 16:57:31 +0100

    inventory fixed 50 percent, playerprefab improved

--- Assets/Scripts/UI Scripts/MenuCanvas/GameOverPanel.cs
+++ Assets/Scripts/UI Scripts/MenuCanvas/GameOverPanel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameOverPanel : MonoBehaviour {
-    public void Continue()
+    public void Restart()
     {
         CommonGameobjects.Instance.goEventSystem.GetComponent<PauseMenuFunction>().RestartLevel();
     }
